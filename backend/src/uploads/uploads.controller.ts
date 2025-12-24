5871bea1e0f91b5fa5230e5fd76386e2b328b243
knage9 <knagh.forall@gmail.com>
2025-12-24 19:34:54 +0300

    add-uploads

---
+++ backend/src/uploads/uploads.controller.ts
@@ -0,0 +1,23 @@
+import { Controller, Post, UseInterceptors, UploadedFile } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
+
+@Controller('uploads')
+export class UploadsController {
+    @Post('images')
+    @UseInterceptors(FileInterceptor('file', {
+        storage: diskStorage({
+            destination: './uploads',
+            filename: (req, file, cb) => {
+                const randomName = Array(32).fill(null).map(() => (Math.round(Math.random() * 16)).toString(16)).join('');
+                return cb(null, `${randomName}${extname(file.originalname)}`);
+            },
+        }),
+    }))
+    uploadFile(@UploadedFile() file: Express.Multer.File) {
+        return {
+            url: `/uploads/${file.filename}`,
+        };
+    }
+}
